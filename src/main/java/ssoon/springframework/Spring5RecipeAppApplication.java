590cd5a6977091b27de5b0edeabbeeb203bee419
Soohoon Lee <shlee@nvista.co.kr>
2019-02-11 17:55:47 +0900

    Initial commit

---
+++ src/main/java/ssoon/springframework/Spring5RecipeAppApplication.java
@@ -0,0 +1,14 @@
+package ssoon.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Spring5RecipeAppApplication.class, args);
+  }
+
+}
+
